eb96063a973cfa0077807c4d2b5cd9bcbe6e3292
Silje Heldal Monsen <Silje24@yahoo.com>
2020-11-30 15:02:57 +0100

    added function to the favourites page

--- js/favourites.js
+++ js/favourites.js
@@ -1,3 +1,26 @@
 /* This page should fetch the array of items stored in localStorage and display them or display a message 
-that there are no items /*
+that there are no items */
 
+(async function array()  {
+
+    const container = document.querySelector(".favourites-container");
+
+    try {
+
+        const response = await fetch(articlesUrl);
+        const json = await response.json();
+
+        if()  {
+             // display the array
+
+        }
+        else {
+            // display a message
+        }
+
+    } catch(error) {
+
+        console.log(error);
+    }
+     
+})();   
